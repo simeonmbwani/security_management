a23bf17ed103198a5dfce055c591fbb3e3bd2342
Simeon Mbwani <simeonmbwani@gmail.com>
2026-08-12 22:25:48 +0200

    Update build.sh to force-update superuser password

--- build.sh
+++ build.sh
@@ -1,23 +1,8 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run database migrations
-python manage.py migrate
-
-# Automatically create a default superuser if none exists
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(is_superuser=True).exists():
-    User.objects.create_superuser('simeonmbwani', 'simeonmbwani@gmail.com', '@A1n2d3y4')
-    print('Superuser created successfully.')
-else:
-    print('Superuser already exists.')
+admin_user, created = User.objects.get_or_create(username='simeonmbwani', defaults={'email': 'simeonmbwani@gmail.com', 'is_staff': True, 'is_superuser': True})
+admin_user.set_password('@A1n2d3y4')
+admin_user.save()
+print('Superuser password ensured.')
 "
-
-# Collect static files (if applicable)
-python manage.py collectstatic --no-input
